tryton/tryton

Add lead time and transit location for internal shipments between warehouses
issue5587
review25331002

location.py
@@ -2,14 +2,19 @@
 # this repository contains the full copyright notices and license terms.
 import datetime
 from decimal import Decimal
-from trytond.model import ModelView, ModelSQL, fields
+
+from sql import Null
+from sql.conditionals import Case
+
+from trytond.model import ModelView, ModelSQL, MatchMixin, fields
 from trytond import backend
 from trytond.pyson import Eval, If
 from trytond.transaction import Transaction
 from trytond.pool import Pool, PoolMeta
 from trytond.tools import grouped_slice
 
-__all__ = ['Location', 'Party', 'ProductsByLocationsContext']
+__all__ = ['Location', 'Party', 'ProductsByLocationsContext',
+    'LocationLeadTime']
 
 STATES = {
     'readonly': ~Eval('active'),
@@ -425,3 +430,37 @@ class ProductsByLocationsContext(ModelView):
         if self.forecast_date is None:
             return datetime.datetime.max
         return self.forecast_date
+
+
+class LocationLeadTime(ModelSQL, ModelView, MatchMixin):
+    'Location Lead Time'
+    __name__ = 'stock.location.lead_time'
+
+    sequence = fields.Integer('Sequence')
+    warehouse_from = fields.Many2One('stock.location', 'Warehouse From',
+        ondelete='CASCADE',
+        domain=[
+            ('type', '=', 'warehouse'),
+            ])
+    warehouse_to = fields.Many2One('stock.location', 'Warehouse To',
+        ondelete='CASCADE',
+        domain=[
+            ('type', '=', 'warehouse'),
+            ])
+    lead_time = fields.TimeDelta('Lead Time')
+
+    @classmethod
+    def __setup__(cls):
+        super(LocationLeadTime, cls).__setup__()
+        cls._order.insert(0, ('sequence', 'ASC'))
+
+    @classmethod
+    def order_sequence(cls, tables):
+        table, _ = tables[None]
+        return [Case((table.sequence == Null, 0), else_=1), table.sequence]
+
+    @classmethod
+    def get_lead_time(cls, pattern):
+        for record in cls.search([]):
+            if record.match(pattern):
+                return record.lead_time

trytonus/stock

location.py
@@ -2,8 +2,12 @@
 # this repository contains the full copyright notices and license terms.
 import datetime
 from decimal import Decimal
-from trytond.model import ModelView, ModelSQL, fields
+from trytond.model import ModelView, ModelSQL, fields, MatchMixin
 from trytond.wizard import Wizard, StateView, Button, StateAction
+
+from sql import Null
+from sql.conditionals import Case
+
 from trytond import backend
 from trytond.pyson import Eval, PYSONEncoder, Date, If
 from trytond.transaction import Transaction
@@ -11,7 +15,7 @@ from trytond.pool import Pool, PoolMeta
 from trytond.tools import grouped_slice
 
 __all__ = ['Location', 'Party', 'ProductsByLocationsStart',
-    'ProductsByLocations']
+    'ProductsByLocations', 'LocationLeadTime']
 
 STATES = {
     'readonly': ~Eval('active'),
@@ -456,3 +460,37 @@ class ProductsByLocations(Wizard):
         action['name'] += ' - (%s) @ %s' % (
             ','.join(l.name for l in locations), date)
         return action, {}
+
+
+class LocationLeadTime(ModelSQL, ModelView, MatchMixin):
+    'Location Lead Time'
+    __name__ = 'stock.location.lead_time'
+
+    sequence = fields.Integer('Sequence')
+    warehouse_from = fields.Many2One('stock.location', 'Warehouse From',
+        ondelete='CASCADE',
+        domain=[
+            ('type', '=', 'warehouse'),
+            ])
+    warehouse_to = fields.Many2One('stock.location', 'Warehouse To',
+        ondelete='CASCADE',
+        domain=[
+            ('type', '=', 'warehouse'),
+            ])
+    lead_time = fields.TimeDelta('Lead Time')
+
+    @classmethod
+    def __setup__(cls):
+        super(LocationLeadTime, cls).__setup__()
+        cls._order.insert(0, ('sequence', 'ASC'))
+
+    @classmethod
+    def order_sequence(cls, tables):
+        table, _ = tables[None]
+        return [Case((table.sequence == Null, 0), else_=1), table.sequence]
+
+    @classmethod
+    def get_lead_time(cls, pattern):
+        for record in cls.search([]):
+            if record.match(pattern):
+                return record.lead_time